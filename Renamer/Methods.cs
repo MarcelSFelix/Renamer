f9ded77b688e3687d6ababcd3caee00085ec54e6
JoRuebsam <jonathan.ruebsam@its-stuttgart.de>
2023-06-15 08:29:57 +0200

    add stuff

--- Renamer/Methods.cs
+++ Renamer/Methods.cs
@@ -8,6 +8,26 @@ namespace Renamer
 {
     public class Methods
     {
+        static void RenameImages(string directoryPath, string prefix)
+        {
+            try
+            {
+                string[] imageFiles = Directory.GetFiles(directoryPath, "*.jpg");
 
+                for (int i = 0; i < imageFiles.Length; i++)
+                {
+                    string oldFilePath = imageFiles[i];
+                    string newFileName = prefix + (i + 1) + ".jpg";
+                    string newFilePath = Path.Combine(directoryPath, newFileName);
+
+                    File.Move(oldFilePath, newFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fehler beim Umbenennen der Bilder: " + ex.Message);
+            }
+        }
     }
 }
+
